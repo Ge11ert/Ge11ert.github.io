bdf5db91f5a25f10068b977581b88e20a0efc1f8
a.abramyants <a.abramyants@fun-box.ru>
2019-06-09 14:12:09 +0300

    Перенос ограничения ширины на элемент page__content.

--- src/components/page/index.js
+++ src/components/page/index.js
@@ -1,6 +1,8 @@
 export { default } from './page.svelte';
 export { default as Page__Stripe } from './__stripe';
+export { default as Page__Content } from './__content';
 
+require('./__content/page__content.scss');
 require('./__stripe/page__stripe.scss');
 
 require('./page.scss');
