8f985a2e00ab63ea62710bec1952fdf8fcbae743
a.abramyants <a.abramyants@fun-box.ru>
2019-06-09 14:14:03 +0300

    Добавление компонента navigation.

---
+++ src/components/navigation/navigation.svelte
@@ -0,0 +1,24 @@
+<script>
+  import mainNavigationItems from 'data/main-navigation-items';
+  let opened = false;
+  export let mods = {};
+  export let mix = [];
+</script>
+
+<nav class="{b('navigation', { mods, mix})}">
+  <a href="#" class="{b('navigation__link')}">
+    Наверх
+  </a>
+  <ul class="{b('navigation__menu')}">
+    {#each mainNavigationItems as {title, href}}
+      <li class="{b('navigation__item')}">
+        <a href="{href}">
+          {title}
+        </a>
+      </li>
+    {/each}
+  </ul>
+  <button class="{b('navigation__button')}">
+    Открыть меню
+  </button>
+</nav>
