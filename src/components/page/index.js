e881bd02ef19cfee16c6d62e4930a37fbb75aeea
a.abramyants <a.abramyants@fun-box.ru>
2019-06-09 14:11:02 +0300

    Задание базовых паддингов через переменные.

--- src/components/page/index.js
+++ src/components/page/index.js
@@ -1,4 +1,6 @@
 export { default } from './page.svelte';
-export {default as Page__Stripe } from './__stripe'
+export { default as Page__Stripe } from './__stripe';
+
+require('./__stripe/page__stripe.scss');
 
 require('./page.scss');
