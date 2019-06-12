f29457ad9b5ed95b286141f90c532fcb20c9b506
a.abramyants <a.abramyants@fun-box.ru>
2019-06-12 19:38:54 +0300

    Вёрстка навигации в шапке.

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -1,1 +1,13 @@
 export { default } from './navigation.svelte';
+
+require('./__button/navigation__button.scss');
+require('./__button-text/navigation__button-text.scss');
+require('./__item/navigation__item.scss');
+require('./__menu/navigation__menu.scss');
+require('./__link/navigation__link.scss');
+require('./__logo-link/navigation__logo-link.scss');
+require('./__logo-text/navigation__logo-text.scss');
+
+require('./navigation.scss');
+
+require('./_opened/navigation_opened.scss');
--- src/components/navigation/navigation.svelte
+++ src/components/navigation/navigation.svelte
@@ -3,22 +3,33 @@
   let opened = false;
   export let mods = {};
   export let mix = [];
+
+  $: navigationMods = Object.assign({ opened }, mods);
+
+  function onClick() {
+    opened = !opened;
+  }
 </script>
 
-<nav class="{b('navigation', { mods, mix})}">
-  <a href="#" class="{b('navigation__link')}">
-    Наверх
+<nav class="{b('navigation', { mods: navigationMods, mix})}">
+  <a href="#" class="{b('navigation__logo-link')}">
+    <span class="{b('navigation__logo-text')}">
+      Наверх
+    </span>
   </a>
   <ul class="{b('navigation__menu')}">
-    {#each mainNavigationItems as {title, href}}
+    {#each mainNavigationItems as {title, link}}
       <li class="{b('navigation__item')}">
-        <a href="{href}">
+        <a href="{link}" class="{b('navigation__link')}">
           {title}
         </a>
       </li>
     {/each}
   </ul>
-  <button class="{b('navigation__button')}">
-    Открыть меню
+  <button
+    class="{b('navigation__button')}"
+    on:click={onClick}
+  >
+    <span class="{b('navigation__button-text')}">Открыть меню</span>
   </button>
 </nav>
