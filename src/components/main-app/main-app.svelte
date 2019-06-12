e24eb940d6849646338841a4f9a659d6f732d4af
a.abramyants <a.abramyants@fun-box.ru>
2019-06-12 21:07:44 +0300

    Баннер в шапке.

--- src/components/main-app/main-app.svelte
+++ src/components/main-app/main-app.svelte
@@ -2,12 +2,14 @@
   import Page, { Page__Stripe, Page__Content } from 'components/page';
   import Header from 'components/header';
   import Navigation from 'components/navigation';
+  import HeaderBanner from 'components/header-banner';
 </script>
 
 <Page>
   <Page__Content>
     <Header mix="{['page__header']}">
       <Navigation mix="{['header__navigation']}"/>
+      <HeaderBanner mix="{['header__banner']}"/>
     </Header>
   </Page__Content>
 </Page>
