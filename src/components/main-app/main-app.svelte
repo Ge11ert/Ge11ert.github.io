ec378dcb854a78e770dd426a5b357de3a365faaa
a.abramyants <a.abramyants@fun-box.ru>
2019-06-12 19:40:49 +0300

    Добавление шапки с навигацией на главную страницу.

--- src/components/main-app/main-app.svelte
+++ src/components/main-app/main-app.svelte
@@ -1,9 +1,13 @@
 <script>
-  import Page, { Page__Stripe } from 'components/page';
+  import Page, { Page__Stripe, Page__Content } from 'components/page';
+  import Header from 'components/header';
+  import Navigation from 'components/navigation';
 </script>
 
 <Page>
-  <Page__Stripe>
-    Todo
-  </Page__Stripe>
+  <Page__Content>
+    <Header mix="{['page__header']}">
+      <Navigation mix="{['header__navigation']}"/>
+    </Header>
+  </Page__Content>
 </Page>
