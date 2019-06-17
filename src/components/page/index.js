00df6337063c5e07964e39f9bb2417d3652e303a
a.abramyants <a.abramyants@fun-box.ru>
2019-06-18 00:26:19 +0300

    Добавление элемента page__main.

--- src/components/page/index.js
+++ src/components/page/index.js
@@ -1,6 +1,7 @@
 export { default } from './page.svelte';
 export { default as Page__Stripe } from './__stripe';
 export { default as Page__Content } from './__content';
+export { default as Page__Main } from './__main';
 
 require('./__content/page__content.scss');
 require('./__stripe/page__stripe.scss');
