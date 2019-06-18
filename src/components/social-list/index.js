c5e0dffcd00a328b1af0d8276217ad71180cfd8e
a.abramyants <a.abramyants@fun-box.ru>
2019-06-18 23:55:35 +0300

    Добавление компонента social-list.

---
+++ src/components/social-list/index.js
@@ -0,0 +1,7 @@
+export { default } from './social-list.svelte';
+
+require('./__item/social-list__item.scss');
+require('./__link/social-list__link.scss');
+require('./__link-text/social-list__link-text.scss');
+
+require('./social-list.scss');
---
+++ src/components/social-list/social-list.svelte
@@ -0,0 +1,28 @@
+<script>
+  import SvgIcon from 'components/svg-icon';
+  import socialList from 'data/social-list';
+  export let mods = {};
+  export let mix = [];
+</script>
+
+<ul class="{b('social-list', { mods, mix })}">
+  {#each socialList as {title, href, icon} }
+    <li class="social-list__item">
+      <a
+        href="{href}"
+        class="social-list__link"
+      >
+        <span class="social-list__link-text">
+          {title}
+        </span>
+
+        <SvgIcon
+          symbolId="{icon}"
+          width="{32}"
+          height="{32}"
+          mix="social-list__icon"
+        ></SvgIcon>
+      </a>
+    </li>
+  {/each}
+</ul>
