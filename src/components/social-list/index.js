32933ddc714cb2499add7ad0c4e24e929fb62713
a.abramyants <a.abramyants@fun-box.ru>
2019-06-21 01:17:24 +0300

    Добавление модификатора блока social-list для секции about-me.

--- src/components/social-list/index.js
+++ src/components/social-list/index.js
@@ -5,3 +5,5 @@ require('./__link/social-list__link.scss');
 require('./__link-text/social-list__link-text.scss');
 
 require('./social-list.scss');
+
+require('./_for/_about-me/social-list_for_about-me.scss');
