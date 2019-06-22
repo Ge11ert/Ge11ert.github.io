a5511ab1cbc3b13a159922854940d5e7d6cffc03
a.abramyants <a.abramyants@fun-box.ru>
2019-06-22 03:09:19 +0300

    Добавление иконок для skills-list.

---
+++ src/data/skills/index.js
@@ -0,0 +1,34 @@
+export default [
+  {
+    title: 'HTML5',
+    icon: require('./html5.svg').default.id,
+  },
+  {
+    title: 'CSS3',
+    icon: require('./css3.svg').default.id,
+  },
+  {
+    title: 'JavaScript',
+    icon: require('./javascript.svg').default.id,
+  },
+  {
+    title: 'SCSS',
+    icon: require('./sass.svg').default.id,
+  },
+  {
+    title: 'Gulp',
+    icon: require('./gulp.svg').default.id,
+  },
+  {
+    title: 'git',
+    icon: require('./git.svg').default.id,
+  },
+  {
+    title: 'БЭМ',
+    icon: require('./bem.svg').default.id,
+  },
+  {
+    title: 'Адаптив',
+    icon: require('./adaptive.svg').default.id,
+  },
+];
