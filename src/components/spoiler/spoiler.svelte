c19a6c44057f4f070ad09295cc427a627ea6060d
a.abramyants <a.abramyants@fun-box.ru>
2019-06-28 01:53:17 +0300

    Добавление компонента spoiler.

---
+++ src/components/spoiler/spoiler.svelte
@@ -0,0 +1,31 @@
+<script>
+  export let mods = {};
+  export let mix = [];
+  export let title = '';
+
+  let expanded = false;
+  const expand = () => {
+    expanded = !expanded;
+  };
+  $: localMods = Object.assign({ expanded }, mods);
+</script>
+
+<div class="{b('spoiler', { mods: localMods, mix})}">
+  <div class="spoiler__title">
+    <span class="spoiler__title-text">
+      {title}
+    </span>
+    <button
+      class="spoiler__expand-button"
+      on:click="{expand}"
+    >
+      <span class="spoiler__button-text">
+        { expanded ? 'Скрыть' : 'Развернуть' }
+      </span>
+    </button>
+  </div>
+
+  <div class="spoiler__content">
+    <slot></slot>
+  </div>
+</div>
