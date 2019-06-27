640d8a9df0449c7232e10217bae1b5003fc63ef9
a.abramyants <a.abramyants@fun-box.ru>
2019-06-28 01:53:51 +0300

    Добавление компонента certificate.

---
+++ src/components/certificate/certificate.svelte
@@ -0,0 +1,20 @@
+<script>
+  export let mods = {};
+  export let mix = [];
+  export let text;
+  export let href;
+  export let imageSrc;
+</script>
+
+<a href="{href}" class="{b('certificate', { mods, mix })}">
+  <span class="certificate__text">
+    {text}
+  </span>
+  <img
+    class="certificate__image"
+    src="{imageSrc}"
+    alt="{text}"
+    title="{text}"
+  >
+</a>
+
